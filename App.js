0f6319d20f0619cadf7023cc8a47888141293d3e
Anurag <myanuragojha@gmail.com>
2019-06-09 18:33:38 +0530

    added sqlite to node_modules

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+const Expenses = {
+  name: 'Expenses',
+  properties: {
+    HouseRent: {type:'int', default:0},
   }
 }
 
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  render() {
+    return (
+     <View style={{padding: 10}}>
+       <TextInput
+          style={{height: 40}}
+          placeholder="Enter House Rent"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button onPress={onPressEnter} title = 'Enter'/>
+        <Text>{this.state.text}</Text>
+        <Button onPress={showDataBase} title = 'Show'/>
+     </View>
+    );
+  }
+}
+onPressEnter = ()=>{
+}
+
+showDataBase = ()=>{
+  console.log('show')
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
